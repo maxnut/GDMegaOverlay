8a9137bc683e5109504eacb56bf88f97436caf21
krep <federico.caprini@protonmail.com>
2022-12-24 12:47:16 +0100

    Removed redundant fstream calls

--- src/Replay.cpp
+++ src/Replay.cpp
@@ -17,12 +17,7 @@ void Replay::Load(std::string name)
         for (size_t _ = 0; _ <= left / 22U; ++_)
         {
             Action a;
-            file.read((char *)&a.press, sizeof(bool));
-            file.read((char *)&a.player2, sizeof(bool));
-            file.read((char *)&a.frame, sizeof(uint32_t));
-            file.read((char *)&a.yAccel, sizeof(double));
-            file.read((char *)&a.px, sizeof(float));
-            file.read((char *)&a.py, sizeof(float));
+            file.read((char *)&a, sizeof(Action));
             AddAction(a);
         }
         file.read((char *)&fps, sizeof(float));
@@ -44,12 +39,7 @@ void Replay::Save(std::string name)
     std::ofstream file("GDMenu/replays/" + name + ".replay", std::ios::out | std::ios::binary);
     for (const auto &a : actions)
     {
-        file.write((char *)&a.press, sizeof(bool));
-        file.write((char *)&a.player2, sizeof(bool));
-        file.write((char *)&a.frame, sizeof(uint32_t));
-        file.write((char *)&a.yAccel, sizeof(double));
-        file.write((char *)&a.px, sizeof(float));
-        file.write((char *)&a.py, sizeof(float));
+        file.write((char *)&a, sizeof(Action));
     }
     file.write((char *)&fps, sizeof(float));
     file.close();
