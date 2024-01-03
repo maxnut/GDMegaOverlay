bda75406b0526fbff9d98ade75eda54c80a25cf7
maxnut <54683950+maxnut@users.noreply.github.com>
2024-01-03 15:53:33 +0100

    fix replay last checkpoint

--- src/Hacks/ReplayLastCheckpoint.cpp
+++ src/Hacks/ReplayLastCheckpoint.cpp
@@ -1,26 +1,48 @@
 #include "ReplayLastCheckpoint.h"
-#include "../utils.hpp"
-#include "../Settings.h"
 #include "../Common.h"
+#include "../Settings.h"
+#include "../utils.hpp"
 
 #include <MinHook.h>
 
-//ok so for whatever reason on windows instead of calling togglepracticemode rob calls resetlevelfromstart. on android he calls togglepracticemode tho :)
+// ok so for whatever reason on windows instead of calling togglepracticemode rob calls resetlevelfromstart. on android
+// he calls togglepracticemode tho :)
 void __fastcall ReplayLastCheckpoint::playLayerResetLevelFromStartHook(void* self, void*)
 {
-    bool replay = Settings::get<bool>("level/replay_checkpoint");
-
-    if(replay)
-    {
-        reinterpret_cast<void(__thiscall*)(cocos2d::CCLayer*)>(utils::gd_base + 0x2E42B0)(Common::getBGL());
-        return;
-    }
+	bool replay = Settings::get<bool>("level/replay_checkpoint");
     
-    playLayerResetLevelFromStart(self);
+
+	if (replay && levelCompleted)
+	{
+		reinterpret_cast<void(__thiscall*)(cocos2d::CCLayer*)>(utils::gd_base + 0x2E42B0)(Common::getBGL());
+        *reinterpret_cast<bool*>(reinterpret_cast<uintptr_t>(Common::getBGL()) + 10868) = true;
+        levelCompleted = false;
+		return;
+	}
+    levelCompleted = false;
+	playLayerResetLevelFromStart(self);
+}
+
+void __fastcall ReplayLastCheckpoint::playLayerLevelCompleteHook(void* self, void*)
+{
+	levelCompleted = true;
+	playLayerLevelComplete(self);
+}
+
+int __fastcall ReplayLastCheckpoint::playLayerResetLevelHook(void* self, void*)
+{
+	levelCompleted = false;
+	return playLayerResetLevel(self);
 }
 
 void ReplayLastCheckpoint::initHooks()
 {
 	MH_CreateHook(reinterpret_cast<void*>(utils::gd_base + 0x2E4210), playLayerResetLevelFromStartHook,
 				  reinterpret_cast<void**>(&playLayerResetLevelFromStart));
+
+    MH_CreateHook(reinterpret_cast<void*>(utils::gd_base + 0x2d7f30), playLayerLevelCompleteHook,
+				  reinterpret_cast<void**>(&playLayerLevelComplete));
+
+    MH_CreateHook(reinterpret_cast<void*>(utils::gd_base + 0x2E42B0), playLayerResetLevelHook,
+				  reinterpret_cast<void**>(&playLayerResetLevel));
 }
--- src/Hacks/ReplayLastCheckpoint.h
+++ src/Hacks/ReplayLastCheckpoint.h
@@ -2,8 +2,17 @@
 
 namespace ReplayLastCheckpoint
 {
+
+inline bool levelCompleted = false;
+
 inline void(__thiscall* playLayerResetLevelFromStart)(void*);
 void __fastcall playLayerResetLevelFromStartHook(void* self, void*);
+
+inline void(__thiscall* playLayerLevelComplete)(void*);
+void __fastcall playLayerLevelCompleteHook(void* self, void*);
+
+inline int(__thiscall* playLayerResetLevel)(void*);
+int __fastcall playLayerResetLevelHook(void* self, void*);
 
 void initHooks();
 } // namespace ReplayLastCheckpoint
