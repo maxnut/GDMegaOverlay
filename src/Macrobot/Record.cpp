8c429bc32bda5a32615d0537d612d8463926180c
maxnut <54683950+maxnut@users.noreply.github.com>
2024-02-12 23:45:34 +0100

    remove unused funcs

--- src/Macrobot/Record.cpp
+++ src/Macrobot/Record.cpp
@@ -50,40 +50,6 @@ class $modify(GJBaseGameLayer)
 	}
 };
 
-std::string narrow(const wchar_t *str)
-{
-	int size = WideCharToMultiByte(CP_UTF8, 0, str, -1, nullptr, 0, nullptr, nullptr);
-	if (size <= 0)
-	{ /* fuck */
-	}
-	auto buffer = new char[size];
-	WideCharToMultiByte(CP_UTF8, 0, str, -1, buffer, size, nullptr, nullptr);
-	std::string result(buffer, size_t(size) - 1);
-	delete[] buffer;
-	return result;
-}
-inline auto narrow(const std::wstring &str)
-{
-	return narrow(str.c_str());
-}
-
-std::wstring widen(const char *str)
-{
-	int size = MultiByteToWideChar(CP_UTF8, 0, str, -1, nullptr, 0);
-	if (size <= 0)
-	{ /* fuck */
-	}
-	auto buffer = new wchar_t[size];
-	MultiByteToWideChar(CP_UTF8, 0, str, -1, buffer, size);
-	std::wstring result(buffer, size_t(size) - 1);
-	delete[] buffer;
-	return result;
-}
-inline auto widen(const std::string &str)
-{
-	return widen(str.c_str());
-}
-
 Recorder::Recorder()
 	: m_width(1280), m_height(720), m_fps(60)
 {}
